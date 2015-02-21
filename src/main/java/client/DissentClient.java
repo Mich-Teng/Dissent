8ebb97f5ce0d0cf1857eb4a5781d3dbfc9ca3998
chaoteng <chao.teng@yale.edu>
2015-02-20 22:58:50 -0500

    Dissent--First Commit

---
+++ src/main/java/client/DissentClient.java
@@ -0,0 +1,14 @@
+package client;
+
+/**
+ * ***************************************************************
+ * Author: Chao Teng
+ * Date: 2015-02-20 22:54.
+ * Package: client
+ * Description:
+ * Welcome to contact chao.teng@yale.edu if you have any questions.
+ * ****************************************************************
+ */
+
+public class DissentClient {
+}
---
+++ src/main/java/server/DissentServer.java
@@ -0,0 +1,14 @@
+package server;
+
+/**
+ * ***************************************************************
+ * Author: Chao Teng
+ * Date: 2015-02-20 22:54.
+ * Package: server
+ * Description:
+ * Welcome to contact chao.teng@yale.edu if you have any questions.
+ * ****************************************************************
+ */
+
+public class DissentServer {
+}
